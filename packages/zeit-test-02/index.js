d3bb6719b064db5fb15e01aeea51ff281247bab6
Maxim <mzhukov31415dev@gmail.com>
2019-04-16 23:22:19 +0300

    Update index.js

--- packages/zeit-test-02/index.js
+++ packages/zeit-test-02/index.js
@@ -7,5 +7,5 @@ const path     = require('path')
 const util     = require('util')
 
 module.exports = (req, res) => {
-  res.end("index.js - done.\n-------\nYes, really done.")
+  res.end("<!doctype html><html lang=en><head><meta charset='utf8'/><title>Zeit Test 2</title></head><body><div><strong>Zeit&trade; Now&trade; builder:</strong>&nbsp;<code>@now/node</code></div><div><strong><code>index.js</code></strong> - done.</div><hr width='80%'><div>Yes, really done.</div></body></html>")
 }
