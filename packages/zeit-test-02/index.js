4d3ccaab5172d2eb1b863698f23ec005e6288ee9
mzhukov1973 <mzhukov31415dev@gmail.com>
2019-04-26 23:10:40 +0300

    Changed title to standard one for Zeit™/GitHub™ test series.

--- packages/zeit-test-02/index.js
+++ packages/zeit-test-02/index.js
@@ -1,10 +1,20 @@
-/*
-  A test of streams, socket-based i/o & ffmpeg.
-*/
+/* A test of streams, socket-based i/o & ffmpeg. */
 
 const fs       = require('fs')
 const path     = require('path')
+const zlib     = require('zlib')
 
-module.exports = (req, res) => {
-  res.end("<!doctype html><html lang=en><head><meta charset='utf8'/><title>UTC-Online Zeit™ Now™ / GitHub™ deployment</title></head><body><div><strong>Zeit&trade; Now&trade; builder:</strong>&nbsp;<code>@now/node</code></div><div><strong><code>index.js</code></strong> - done.</div><hr width='80%'><div>Yes, really done.</div></body></html>")
+const htmlOutput = "<!doctype html>\n<html lang=en>\n <head>\n  <meta charset='utf8'/>\n  <title>Zeitβ|²: UTC-Online Zeit™ Now™ / GitHub™ deployment</title>\n </head>\n <body>\n  <div><strong>Zeit&trade; Now&trade; builder:</strong>&nbsp;<code>@now/node</code></div>\n  <div><strong><code>index.js</code></strong> - done.</div>\n  <hr width='100%'>\n  <div>Yes, really done.</div>\n </body>\n</html>\n"
+
+const gzip = zlib.createGzip()
+//const inp = fs.createReadStream('input.txt')
+//const out = fs.createWriteStream('input.txt.gz')
+//inp.pipe(gzip).pipe(out)
+
+
+
+
+
+module.exports = (req,res) => {
+  res.end(htmlOutput)
 }
